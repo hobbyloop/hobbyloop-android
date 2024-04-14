b8bbd7c5be05d721c6a4d8de3110f6717377dde1
meenjoon <zqudwnsz@naver.com>
2024-04-14 15:51:58 +0900

    feat: buildLogic - Feature 모듈 의존성 구성

--- feature/storage/build.gradle.kts
+++ feature/storage/build.gradle.kts
@@ -1,53 +1,11 @@
 plugins {
-    alias(libs.plugins.androidLibrary)
-    alias(libs.plugins.kotlinAndroid)
+    id("buildlogic.featuremodule")
 }
 
 android {
     namespace = "com.hobboyloopapp.feature.storage"
-    compileSdk = 34
-
-    defaultConfig {
-        minSdk = 24
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
-    buildFeatures {
-        compose = true
-    }
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.1"
-    }
-    packaging {
-        resources {
-            excludes += "/META-INF/{AL2.0,LGPL2.1}"
-        }
-    }
 }
 
 dependencies {
-    implementation(platform(libs.compose.bom))
-    implementation(libs.bundles.ui)
-    implementation(libs.bundles.navigation)
-    testImplementation(libs.bundles.test)
-    androidTestImplementation(libs.bundles.androidTest)
-    debugImplementation(libs.bundles.debugImple)
-
     implementation(project(":core:ui"))
 }
