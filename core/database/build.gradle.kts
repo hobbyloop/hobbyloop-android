62300e1c3d4de6e2b1654a64eb8ae5f4295df90f
meenjoon <zqudwnsz@naver.com>
2024-04-14 15:59:21 +0900

    feat: 추후 각 모듈의 의존성을 build-logic 모듈을 통해 관리하도록 변경

--- core/database/build.gradle.kts
+++ core/database/build.gradle.kts
@@ -1,46 +1,15 @@
 plugins {
-    alias(libs.plugins.androidApplication)
+    alias(libs.plugins.androidLibrary)
     alias(libs.plugins.kotlinAndroid)
 }
 
 android {
     namespace = "com.hobbyloopapp.core.database"
     compileSdk = 34
-
-    defaultConfig {
-        applicationId = "com.hobbyloopapp.core.database"
-        minSdk = 24
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
 }
 
 dependencies {
-
-    implementation(libs.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.test.ext.junit)
-    androidTestImplementation(libs.espresso.core)
+    /**
+     * 추후 라이브러리 의존성 관리를 build-logic 모듈에서 하도록 함
+     */
 }
