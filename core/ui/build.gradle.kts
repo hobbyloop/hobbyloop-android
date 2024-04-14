b3af8c31d002c2b97a0acbed27b25587770be392
meenjoon <zqudwnsz@naver.com>
2024-04-14 15:54:20 +0900

    feat: buildLogic - UI 모듈 의존성 구성

--- core/ui/build.gradle.kts
+++ core/ui/build.gradle.kts
@@ -1,46 +1,11 @@
 plugins {
-    alias(libs.plugins.androidApplication)
-    alias(libs.plugins.kotlinAndroid)
+    id("buildlogic.uimodule")
 }
+
 
 android {
     namespace = "com.hobbyloopapp.core.ui"
-    compileSdk = 34
-
-    defaultConfig {
-        applicationId = "com.hobbyloopapp.core.ui"
-        minSdk = 24
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
 }
 
 dependencies {
-
-    implementation(platform(libs.compose.bom))
-    implementation(libs.bundles.ui)
-    implementation(libs.bundles.navigation)
-    testImplementation(libs.bundles.test)
-    androidTestImplementation(libs.bundles.androidTest)
-    debugImplementation(libs.bundles.debugImple)
 }
