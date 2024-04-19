e44ac29abb87eb2de49a4ae4b85ce9df473b1d34
meenjoon <zqudwnsz@naver.com>
2024-04-19 22:12:06 +0900

    feat: buildLogic - App 모듈 의존성 구성

---
+++ build-logic/src/main/kotlin/internal/Compose.kt
@@ -0,0 +1,29 @@
+package internal
+
+import com.android.build.api.dsl.CommonExtension
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.configure
+import org.gradle.kotlin.dsl.dependencies
+
+internal inline fun <reified T : CommonExtension<*, *, *, *, *>> Project.configureCompose() {
+    extensions.configure<T> {
+        buildFeatures {
+            compose = true
+        }
+        composeOptions {
+            kotlinCompilerExtensionVersion = "1.5.1"
+        }
+    }
+
+    dependencies {
+        add("implementation", platform(getLibrary("compose-bom")))
+        add("implementation", getLibrary("ui"))
+        add("implementation", getLibrary("ui-graphics"))
+        add("implementation", getLibrary("ui-tooling-preview"))
+        add("implementation", getLibrary("material3"))
+        add("implementation", getLibrary("material"))
+        add("implementation", getLibrary("lifecycle-runtime-ktx"))
+        add("implementation", getLibrary("activity-compose"))
+        add("debugImplementation", getLibrary("ui-tooling"))
+    }
+}
--- build.gradle.kts
+++ build.gradle.kts
@@ -2,8 +2,10 @@
 @Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
 plugins {
     alias(libs.plugins.androidApplication) apply false
+    alias(libs.plugins.androidLibrary) apply false
     alias(libs.plugins.kotlinAndroid) apply false
     alias(libs.plugins.org.jetbrains.kotlin.jvm) apply false
-    alias(libs.plugins.androidLibrary) apply false
+    alias(libs.plugins.ktlint) apply true
+    alias(libs.plugins.hilt) apply false
 }
 true // Needed to make the Suppress annotation work for the plugins block
