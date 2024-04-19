f5251110815e18597ab4d20315bb14881f24394f
meenjoon <zqudwnsz@naver.com>
2024-04-19 21:30:09 +0900

    feat: build-logic 모듈 생성

---
+++ build-logic/build.gradle.kts
@@ -0,0 +1,15 @@
+plugins {
+    `kotlin-dsl`
+}
+
+group = "com.hobbyloop.buildlogic"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
+dependencies {
+    compileOnly(libs.android.tools.build.gradle.plugin)
+    compileOnly(libs.kotlin.gradle.plugin)
+}
