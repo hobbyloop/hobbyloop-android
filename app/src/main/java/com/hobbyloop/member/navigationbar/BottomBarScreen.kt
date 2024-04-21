489add0c5ac240c1809e26b7fb93d8de2871eac2
meenjoon <zqudwnsz@naver.com>
2024-04-21 23:03:13 +0900

    refactor: Center,Facility 네이밍 통일화를 위한 이름 변경

--- app/src/main/java/com/hobbyloop/member/navigationbar/BottomBarScreen.kt
+++ app/src/main/java/com/hobbyloop/member/navigationbar/BottomBarScreen.kt
@@ -20,7 +20,7 @@ enum class BottomBarScreen(
     val backgroundColor: Color,
 ) {
     HOME(HOME_GRAPH_ROUTE, R.string.bt_home, 6.dp, R.drawable.bt_home_ic, Color.Cyan.copy(alpha = 0.1f)),
-    FACILITY(CENTER_GRAPH_ROUTE, R.string.bt_center, 6.dp, R.drawable.bt_center_ic, Color.Gray.copy(alpha = 0.1f)),
+    CENTER(CENTER_GRAPH_ROUTE, R.string.bt_center, 6.dp, R.drawable.bt_center_ic, Color.Gray.copy(alpha = 0.1f)),
     RESERVATION(RESERVATION_GRAPH_ROUTE, R.string.bt_reservation, 4.dp, R.drawable.bt_reservation_ic, Color.Red.copy(alpha = 0.1f)),
     SCHEDULE(STORAGE_GRAPH_ROUTE, R.string.bt_schedule, 6.dp, R.drawable.bt_schedule_ic, Color.Blue.copy(alpha = 0.1f)),
     MY(MY_PAGE_GRAPH_ROUTE, R.string.bt_my, 6.dp, R.drawable.bt_my_ic, Color.Yellow.copy(alpha = 0.1f)),
