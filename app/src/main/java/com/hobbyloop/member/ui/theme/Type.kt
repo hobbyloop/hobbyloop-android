6d692209c08a19b146e3f53840f3870337dd916b
KDW03 <asasa0628@gs.cwnu.ac.kr>
2024-04-24 19:59:04 +0900

    feat: pretendard font 구현 #14

--- app/src/main/java/com/hobbyloop/member/ui/theme/Type.kt
+++ app/src/main/java/com/hobbyloop/member/ui/theme/Type.kt
@@ -1,43 +1,72 @@
 package com.hobbyloop.member.ui.theme
 
 import androidx.compose.material3.Typography
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextGeometricTransform
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.sp
+import com.hobbyloop.member.R
+import com.hobbyloop.ui.R.*
 
 // Set of Material typography styles to start with
+// todo fontFamily 적용
 val Typography =
     Typography(
         bodyLarge =
-            TextStyle(
-                fontFamily = FontFamily.Default,
-                fontWeight = FontWeight.Normal,
-                fontSize = 16.sp,
-                lineHeight = 24.sp,
-                letterSpacing = 0.5.sp,
-            ),
+        TextStyle(
+            fontFamily = FontFamily.Default,
+            fontWeight = FontWeight.Normal,
+            fontSize = 16.sp,
+            lineHeight = 24.sp,
+            letterSpacing = 0.5.sp,
+        ),
+        // loginButton ...
         labelLarge = TextStyle(
-            fontFamily = FontFamily.SansSerif,
+            fontFamily =FontFamily(Font(font.pretendard)),
             fontSize = 14.sp,
             fontWeight = FontWeight.SemiBold,
             lineHeight = TextUnit.Unspecified
         ),
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-     */
+        // SingUpTitle ...
+        titleLarge = TextStyle(
+            fontFamily =FontFamily(Font(font.pretendard)),
+            fontSize = 22.sp,
+            fontStyle = FontStyle.Normal,
+            fontWeight = FontWeight.W700,
+        ),
+        // SingUpSubTitle
+        titleMedium = TextStyle(
+            fontFamily =FontFamily(Font(font.pretendard_bold)),
+            fontSize = 18.sp,
+            fontStyle = FontStyle.Normal,
+            fontWeight = FontWeight.W500,
+        ),
+        titleSmall = TextStyle(
+            fontFamily =FontFamily(Font(font.pretendard_bold)),
+            fontSize = 16.sp,
+            fontStyle = FontStyle.Normal,
+            fontWeight = FontWeight.W500,
+        ),
+
+        /* Other default text styles to override
+        titleLarge = TextStyle(
+            fontFamily = FontFamily.Default,
+            fontWeight = FontWeight.Normal,
+            fontSize = 22.sp,
+            lineHeight = 28.sp,
+            letterSpacing = 0.sp
+        ),
+        labelSmall = TextStyle(
+            fontFamily = FontFamily.Default,
+            fontWeight = FontWeight.Medium,
+            fontSize = 11.sp,
+            lineHeight = 16.sp,
+            letterSpacing = 0.5.sp
+        )
+         */
     )
