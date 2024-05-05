8fb4a56abc27695f8eae0bae235f0a5ce0227d17
박종민 <ruho646010@gmail.com>
2024-05-06 00:39:40 +0900

    feat: Home 화면 컴포넌트 UiModel 정의  #12

---
+++ feature/home/src/main/java/com/hobbyloop/feature/home/model/UpcomingReservation.kt
@@ -0,0 +1,11 @@
+package com.hobbyloop.feature.home.model
+
+data class UpcomingReservationUiModel(
+    val centerLogoUrl: String = "",
+    val className: String = "",
+    val centerName: String = "",
+    val teacherName: String = "",
+    val classPeriod: String = "",
+    val date: String = "",
+    val qr: String = "",
+)
