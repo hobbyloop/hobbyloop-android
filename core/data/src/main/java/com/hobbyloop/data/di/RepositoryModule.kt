8bcd80fdcdcce5e4cca87cac98fd69419374ddf8
KDW03 <asasa0628@gs.cwnu.ac.kr>
2024-05-26 01:16:05 +0900

    feat : Center Data Hilt 및 RepositoryImp 구현  #22

--- core/data/src/main/java/com/hobbyloop/data/di/RepositoryModule.kt
+++ core/data/src/main/java/com/hobbyloop/data/di/RepositoryModule.kt
@@ -1,12 +1,18 @@
 package com.hobbyloop.data.di
 
+import com.hobbyloop.data.repository.fake.FakeCenterRepository
+import com.hobbyloop.domain.repository.CenterRepository
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 
+
 @Module
 @InstallIn(SingletonComponent::class)
 abstract class RepositoryModule {
+    @Binds
+    abstract fun bindCenterRepository(centerRepository: FakeCenterRepository): CenterRepository
 
 }
+
---
+++ core/data/src/main/java/com/hobbyloop/data/repository/CenterRepositoryIml.kt
@@ -0,0 +1,18 @@
+package com.hobbyloop.data.repository
+
+import androidx.paging.PagingData
+import com.hobbyloop.domain.model.Center
+import com.hobbyloop.domain.repository.CenterRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOf
+import javax.inject.Inject
+
+//todo network add
+
+/*
+class CenterRepositoryImpl @Inject constructor() : CenterRepository {
+    override fun getCenters(): Flow<PagingData<Center>> {
+        return flowOf(PagingData.empty())
+    }
+
+}*/
