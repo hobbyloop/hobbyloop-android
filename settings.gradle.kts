3905a912f565677d08e50b6b7680e49bd36dc597
meenjoon <zqudwnsz@naver.com>
2024-06-21 00:29:35 +0900

    feat: Android Studio Iguana | 2023.2.1 Canary 7 버전에서 빌드 시 발생하는 이슈 해결

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,16 @@
+/**
+ * Android Studio Iguana | 2023.2.1 Canary 7 버전에서 빌드 시 발생하는 문제를 해결하기 위한 코드 추가
+ *
+ * [이슈]
+ * FAILURE: Build failed with an exception.
+ * * What went wrong:
+ * Unable to make progress running work. There are items queued for execution but none of them can be started
+ *
+ * [참고]
+ * https://stackoverflow.com/questions/77279080/unable-to-make-progress-running-work-android-studio
+ */
+gradle.startParameter.excludedTaskNames.addAll(listOf(":build-logic:testClasses"))
+
 pluginManagement {
     repositories {
         google()
